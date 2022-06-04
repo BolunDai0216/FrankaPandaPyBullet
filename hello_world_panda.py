ea667f3ae56594c5a110cdff7a9ced2e41e77089
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2022-06-03 22:23:26 -0400

    added comments to hello_world_panda

--- hello_world_panda.py
+++ hello_world_panda.py
@@ -8,31 +8,44 @@ import numpy as np
 def main():
     client = p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    p.setGravity(0, 0, 0)
+    p.setGravity(0, 0, -9.81)
 
+    # Load plane
     planeID = p.loadURDF("plane.urdf")
+
+    # Load Franka Panda Robot
     robotID = p.loadURDF("./franka_panda/panda.urdf", useFixedBase=True)
 
+    # Get number of joints
     n_j = p.getNumJoints(robotID)
 
     debug_sliders = []
     joint_ids = []
+
     for i in range(n_j):
+        # get info of each joint
         _joint_infos = p.getJointInfo(robotID, i)
+
         if _joint_infos[2] != p.JOINT_FIXED:
+            # Add a debug slider for all non-fixed joints
             debug_sliders.append(
                 p.addUserDebugParameter(
-                    _joint_infos[1].decode("UTF-8"),
-                    _joint_infos[8],
-                    _joint_infos[9],
-                    0.0,
+                    _joint_infos[1].decode("UTF-8"),  # Joint Name
+                    _joint_infos[8],  # Lower Joint Limit
+                    _joint_infos[9],  # Upper Joint Limit
+                    0.0,  # Default Joint Angle
                 )
             )
+
+            # Save the non-fixed joint IDs
             joint_ids.append(_joint_infos[0])
 
     while True:
         for slider_id, joint_id in zip(debug_sliders, joint_ids):
+            # Get joint angle from debug slider
             _joint_angle = p.readUserDebugParameter(slider_id)
+
+            # Apply joint angle to robot
             p.resetJointState(robotID, joint_id, _joint_angle)
 
         p.stepSimulation()
