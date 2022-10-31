7f1a6ef31d2d0c1e33df37a3d0a1e59b8e6073b1
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2022-10-31 17:29:45 -0400

    added pose control

--- panda_pose_control.py
+++ panda_pose_control.py
@@ -1,4 +1,6 @@
+import copy
 import pathlib
+from pdb import set_trace
 
 import numpy as np
 import numpy.linalg as LA
@@ -9,6 +11,30 @@ from pinocchio.robot_wrapper import RobotWrapper
 from scipy.spatial.transform import Rotation as R
 
 from controllers.utils import get_state_update_pinocchio, send_joint_command
+
+
+def alpha_func(t, T=5.0):
+    if t <= T:
+        alpha = np.sin((np.pi / 4) * (1 - np.cos(np.pi * t / T)))
+        dalpha = (
+            ((np.pi ** 2) / (4 * T))
+            * np.cos((np.pi / 4) * (1 - np.cos(np.pi * t / T)))
+            * np.sin(np.pi * t / T)
+        )
+    else:
+        alpha = 1.0
+        dalpha = 0.0
+
+    return alpha, dalpha
+
+
+def axis_angle_from_rot_mat(rot_mat):
+    rotation = R.from_matrix(rot_mat)
+    axis_angle = rotation.as_rotvec()
+    angle = LA.norm(axis_angle)
+    axis = axis_angle / angle
+
+    return axis, angle
 
 
 def main():
@@ -69,33 +95,56 @@ def main():
     for i, joint_ang in enumerate(target_joint_angles):
         p.resetJointState(robotID, active_joint_ids[i], joint_ang, 0.0)
 
-    init = True
-    target_position = np.array([[-0.3], [-0.4], [0.5]])
+    p_end = np.array([[-0.3], [-0.4], [0.5]])
+
+    # get initial rotation and position
+    q, dq = get_state_update_pinocchio(robot, robotID)
+    R_start = copy.deepcopy(robot.data.oMf[FRAME_ID].rotation)
+    _p_start = robot.data.oMf[FRAME_ID].translation
+    p_start = _p_start[:, np.newaxis]
+
+    # Get target orientation based on initial orientation
+    _R_end = (
+        R.from_euler("x", 45, degrees=True).as_matrix()
+        @ R.from_euler("y", -40, degrees=True).as_matrix()
+        @ R_start
+    )
+    R_end = R.from_matrix(_R_end).as_matrix()
+    R_error = R_end @ R_start.T
+    axis_error, angle_error = axis_angle_from_rot_mat(R_error)
 
     for i in range(80000):
         # Update pinocchio model and get joint states
         q, dq = get_state_update_pinocchio(robot, robotID)
 
-        # Get end-effector position
-        _ee_position = robot.data.oMf[FRAME_ID].translation
-        ee_position = _ee_position[:, np.newaxis]
+        # Get simulation time
+        sim_time = i * (1 / 240)
 
-        # Get target orientation based on initial orientation
-        if init:
-            _init_rotation = robot.data.oMf[FRAME_ID].rotation  # R10
-            _target_rotation = (
-                R.from_euler("x", 45, degrees=True).as_matrix()
-                @ R.from_euler("y", 90, degrees=True).as_matrix()
-                @ _init_rotation
-            )  # R20
-            target_rotation = R.from_matrix(_target_rotation)
-            init = False
+        # Compute α and dα
+        alpha, dalpha = alpha_func(sim_time, T=30.0)
+
+        # Compute p_target
+        p_target = p_start + alpha * (p_end - p_start)
+
+        # Compute v_target
+        v_target = dalpha * (p_end - p_start)
+
+        # Compute R_target
+        theta_t = alpha * angle_error
+        R_target = R.from_rotvec(axis_error * theta_t).as_matrix() @ R_start
+
+        # Compute ω_target
+        ω_target = dalpha * axis_error * angle_error
+
+        # Get end-effector position
+        _p_current = robot.data.oMf[FRAME_ID].translation
+        p_current = _p_current[:, np.newaxis]
 
         # Get end-effector orientation
-        _ee_orientation = robot.data.oMf[FRAME_ID].rotation
+        R_current = robot.data.oMf[FRAME_ID].rotation
 
         # Error rotation matrix
-        R_err = target_rotation.as_matrix() @ _ee_orientation.T
+        R_err = R_target @ R_current.T
 
         # Orientation error in axis-angle form
         rotvec_err = R.from_matrix(R_err).as_rotvec()
@@ -114,11 +163,19 @@ def main():
         G = robot.gravity(q)
 
         # Compute controller
-        target_dx = np.zeros((6, 1))
-        target_dx[:3] = 1.0 * (target_position - ee_position)
-        target_dx[3:] = np.diag([1.0, 1.0, 1.0]) @ rotvec_err[:, np.newaxis]
+        delta_x = np.zeros((6, 1))
+        delta_x[:3] = p_target - p_current
+        delta_x[3:] = rotvec_err[:, np.newaxis]
+        delta_q = pinv_jac @ delta_x
 
-        tau = (pinv_jac @ target_dx - dq[:, np.newaxis]) + G[:, np.newaxis]
+        dx = np.zeros((6, 1))
+        dx[:3] = v_target
+        dx[3:] = ω_target[:, np.newaxis]
+        delta_dq = pinv_jac @ dx - dq[:, np.newaxis]
+        Kp = 3 * np.eye(9)
+        Kd = np.eye(9)
+
+        tau = (Kp @ delta_q + Kd @ delta_dq) + G[:, np.newaxis]
 
         # Set control for the two fingers to zero
         tau[-1] = 0.0
@@ -130,7 +187,7 @@ def main():
         if i % 500 == 0:
             print(
                 "Iter {:.2e} \t ǁeₒǁ₂: {:.2e} \t ǁeₚǁ₂: {:.2e}".format(
-                    i, LA.norm(rotvec_err), LA.norm(target_position - ee_position)
+                    i, LA.norm(rotvec_err), LA.norm(p_target - p_current)
                 ),
             )
 
